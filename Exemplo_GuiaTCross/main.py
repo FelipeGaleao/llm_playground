84b9b2d26e4d5c8fc74c789ff2a2f02752a71205
Maycon Felipe Mota <mayconfelipemotamw3@gmail.com>
2025-06-09 21:12:36 -0400

    fix: ajuste no streamlit

--- Exemplo_GuiaTCross/main.py
+++ Exemplo_GuiaTCross/main.py
@@ -5,7 +5,12 @@ Seguindo princípios de Clean Architecture
 import sys
 import os
 import subprocess
-from pathlib import Path
+import pathlib
+import shutil
+import logging
+from bs4 import BeautifulSoup
+import streamlit as st
+
 
 
 def main():
@@ -37,5 +42,34 @@ def main():
         print("\n🛑 Aplicação interrompida pelo usuário")
 
 
+def inject_ga():
+    # replace G-XXXXXXXXXX to your web app's ID
+    analytics_js = """
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-TF6PYQCV15"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-TF6PYQCV15');
+    </script>
+    <div id="G-TF6PYQCV15"></div>
+    """
+    analytics_id = "G-TF6PYQCV15"
+
+    
+    # Identify html path of streamlit
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=analytics_id): # if id not found within html file
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # backup recovery
+        else:
+            shutil.copy(index_path, bck_index)  # save backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + analytics_js) 
+        index_path.write_text(new_html) # insert analytics tag at top of head
 if __name__ == "__main__":
     main()
